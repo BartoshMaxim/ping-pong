10f64d67c0c8bd29a08d5c88d9e89e148ca3ff5f
manifest16 <35853920+manifest16@users.noreply.github.com>
2018-01-27 11:23:48 +0200

    Platforms control, ball moving

---
+++ Ping-pong/Assets/BallMovement.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+    
+	void Start () {
+        gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(3,0,3), ForceMode.Impulse);
+    }
+}
---
+++ Ping-pong/Assets/LevelController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+
+    public Transform ball;
+    public Vector2 fieldSize;
+    public Platform[] platform;
+
+
+    public KeyCode PlayerOneLeft;
+    public KeyCode PlayerOneRight;
+    public KeyCode PlayerTwoLeft;
+    public KeyCode PlayerTwoRight;
+    public KeyCode PlayerThreeLeft;
+    public KeyCode PlayerThreeRight;
+    public KeyCode PlayerFourLeft;
+    public KeyCode PlayerFourRight;
+
+    private void Start()
+    {
+        foreach (Platform p in platform)
+        {
+            p.GetComponent<Platform>().Speed = 6f;
+        }
+    }
+
+    private void Update()
+    {
+        platform[0].MovePlatform(PlayerOneLeft, PlayerOneRight);
+        platform[1].MovePlatform(PlayerTwoLeft, PlayerTwoRight);
+        platform[2].MovePlatform(PlayerThreeLeft, PlayerThreeRight);
+        platform[3].MovePlatform(PlayerFourLeft, PlayerFourRight);
+
+        if (ball.position.x > fieldSize.x / 2 || ball.position.x < -fieldSize.x / 2 || ball.position.z > fieldSize.y / 2 || ball.position.z < -fieldSize.y / 2)
+        {
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(fieldSize.x, 1, fieldSize.y));
+    }
+}
